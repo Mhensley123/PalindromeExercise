c059795ab5c3ddc1fc1ad335acd65fe4f0d61775
Maria Hensley <mscelie34@zoho.com>
2023-11-02 22:24:16 -0500

    done

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Linq;
+
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var reversedWord = word.Reverse();
+
+            return word.ToLower().SequenceEqual(reversedWord);
+        }
+    }
+}
